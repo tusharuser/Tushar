186f94ae396a505d821a5a959c7e7f74a6d7dbb0
Hertzian1 <86573513+Hertzian1@users.noreply.github.com>
2021-09-27 22:53:21 +0530

    Update __main__.py

--- Hertz/__main__.py
+++ Hertz/__main__.py
@@ -1,6 +1,6 @@
 
-from Sibyl_System import SIBYL, Sibyl_logs, API_ID_KEY, API_HASH_KEY, STRING_SESSION, System
-from Sibyl_System.strings import on_string
+from Hertz import SIBYL, Sibyl_logs, API_ID_KEY, API_HASH_KEY, STRING_SESSION, System
+from Hertz.strings import on_string
 from telethon import TelegramClient, events
 from telethon.sessions import StringSession
 import logging
@@ -10,7 +10,7 @@ import importlib
 logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s',
                     level=logging.WARNING)
 
-from Sibyl_System.plugins import to_load
+from Hertz.systema import to_load
 
 HELP = {}
 IMPORTED = {}
